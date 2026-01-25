eae5a31bf1f59f4351f0b9bbbb493d0b20bec190
Suyog Tandel <git@suyogtandel.in>
2026-01-25 16:05:38 +0530

    chore(nix): add mold linker to shell.nix to improve linking speeds and also fix libcanberra errors

--- shell.nix
+++ shell.nix
@@ -13,6 +13,7 @@ let
     libsoup_3
     pango
     webkitgtk_4_1
+    libcanberra-gtk3
     pcsclite
     hidapi
     mesa 
@@ -29,6 +30,7 @@ let
     
     # Development tools
     rustc
+    mold
     cargo
     deno
     nodejs_22
@@ -45,6 +47,8 @@ let
     libsoup_3
     pango
     webkitgtk_4_1
+    libcanberra-gtk3
+    packagekit
     
     # Hardware
     pcsclite
@@ -57,7 +61,9 @@ pkgs.mkShell {
 
   shellHook = ''
     export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+    export GTK_PATH=${pkgs.libcanberra-gtk3}/lib/gtk-3.0:${pkgs.packagekit}/lib/gtk-3.0:$GTK_PATH
     export XDG_DATA_DIRS=$GSETTINGS_SCHEMAS_PATH:$XDG_DATA_DIRS
+    export RUSTFLAGS="-C link-arg=-fuse-ld=mold"
     
     # Try to uncomment the following lines if you encounter EGL_BAD_PARAMETER errors:
     # export LIBGL_ALWAYS_SOFTWARE=1
